3af45b86e190638ce1b2dd2d81f526706b1225ed
Stuart Bennett <bennettstuart92@gmail.com>
2019-01-04 23:06:00 +0000

    basic auth flow, awaiting parameter extraction

---
+++ src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <button v-on:click="login">Login</button>
+</template>
+
+<script>
+import router from "@/router";
+export default {
+  name: "Login",
+  props: {
+    msg: String
+  },
+  methods: {
+    login: () => {
+      const clientId = process.env.VUE_APP_CLIENT_ID;
+      const redirectUri = encodeURIComponent(process.env.VUE_APP_REDIRECT_URI);
+
+      console.log(process.env.VUE_APP_REDIRECT_URI, redirectUri);
+
+      window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=user-read-private%20user-read-email&state=34fFs29kd09`;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    artists: [],
+    artist: null,
+    albums: [],
+    album: null,
+    playTrack: null,
+    refreshToken: null
+  },
   mutations: {},
   actions: {}
 });
